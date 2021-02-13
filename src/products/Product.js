90d84b67ce080335a980c03667a9e7b287ee4316
Alice Boone <alicegbone@gmail.com>
2021-02-12 16:32:00 -0800

    fixing styles

--- src/products/Product.js
+++ src/products/Product.js
@@ -8,21 +8,11 @@ const Product = (props) => {
       <div className="product-card__container" >
       <img src={props.product.imageUrl1} alt='' className="product-card__img" />
       <div className="product-card__content">
-        <h1>{props.product.playerName}</h1>
-        <h2>{props.product.releaseYear}</h2>
+        <h4>{props.product.playerName}</h4>
+        <h5>{props.product.releaseYear}</h5>
         <Link to={`/product/${props.product.id}`}> Select</Link>
       </div>
     </div>
-
-      // <Card style={{ width: "18rem" }}>
-      //   <Card.Body>
-  
-      //     <Card.Img variant="top" src={props.product.imageUrl1} />
-      //     <Card.Title>{props.product.playerName}</Card.Title>
-      //     <Card.Title>{props.product.releaseYear}</Card.Title>
-      //     <Link to={`/product/${props.product.id}`}> Select</Link>
-      //   </Card.Body>
-      // </Card>
     );
 };
 
